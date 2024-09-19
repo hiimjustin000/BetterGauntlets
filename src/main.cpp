8efe4828e978ea9de69391d818a7f41c1f1f5938
Jay <168689600+JayDasshu@users.noreply.github.com>
2024-09-19 00:53:21 -0400

    quick positioning work

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,21 @@ class $modify(RedesignedGauntletLayer, GauntletLayer) {
 		backgroundSprite->setID("gauntlet-background-sprite");
 		backgroundSprite->setColor(ccc3(51, 16, 16));
 
-		auto gauntletTitleText = static_cast<CCLabelBMFont*>(this->getChildren()->objectAtIndex(1));
+		// this is the shadow text for whatever fuckall reason
+		auto gauntletTitleText = static_cast<CCLabelBMFont*>(this->getChildByIDRecursive("title"));
 		gauntletTitleText->setFntFile("GR_OxygeneFont.fnt"_spr);
 		gauntletTitleText->setColor(ccc3(0, 0, 0));
 		gauntletTitleText->setScale(0.50);
+		gauntletTitleText->setPositionY(287);
+		gauntletTitleText->setPositionX(286.50);
 		
+		// and this is the actual title text like what dude
 		auto gauntletShadowText = static_cast<CCLabelBMFont*>(this->getChildByIDRecursive("title-shadow"));
 		gauntletShadowText->setFntFile("GR_OxygeneFont.fnt"_spr);
 		gauntletShadowText->setColor(ccc3(255, 255, 255));
 		gauntletShadowText->setScale(0.50);
+		gauntletShadowText->setPositionY(290);
+		gauntletShadowText->setPositionX(284.50);
 
 		// for (int i = 2; i <= 38; i++) { // ok idk why the fuck 38 works but it works
 			// auto circleSprite = static_cast<CCSprite*>(this->getChildren()->objectAtIndex(i));
