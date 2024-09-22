4c3945a6b86976d3018053a03f4c7895bd2e05e4
Capeling <113705504+Capeling@users.noreply.github.com>
2024-09-22 10:18:01 +0930

    I FUCKING HATE BOOMSCROLLLAYER

--- src/GauntletSelectLayer.cpp
+++ src/GauntletSelectLayer.cpp
@@ -1,8 +1,76 @@
 #include <Geode/modify/GauntletSelectLayer.hpp>
+#include <algorithm>
 
 using namespace geode::prelude;
 
-class $modify(GauntletSelectayer) {
-    pageCircleVis->getChildByID("")
-	}
-}
+class $modify(RedesignedGauntletSelectLayer, GauntletSelectLayer) {
+    struct Fields {
+        std::vector<CCMenuItemSpriteExtra*> m_dots = {};
+        CCMenu* m_dotsMenu = nullptr;
+    };
+
+    void updateDots() {
+        auto sfc = CCSpriteFrameCache::sharedSpriteFrameCache();
+
+        for(CCMenuItemSpriteExtra* btn : m_fields->m_dots) {
+            auto btnIdx = std::find(m_fields->m_dots.begin(), m_fields->m_dots.end(), btn) - m_fields->m_dots.begin();
+
+            int newPage = m_scrollLayer->m_page;
+            if(newPage <= -1) newPage = m_fields->m_dots.size() + newPage;
+            if(newPage >= m_fields->m_dots.size()) newPage = newPage % m_fields->m_dots.size();
+            log::info("{}, {}", btnIdx, newPage);
+
+            if(btnIdx == newPage)
+                static_cast<CCSprite*>(btn->getNormalImage())->setDisplayFrame(sfc->spriteFrameByName("gj_navDotBtn_on_001.png"));
+            else
+                static_cast<CCSprite*>(btn->getNormalImage())->setDisplayFrame(sfc->spriteFrameByName("gj_navDotBtn_off_001.png"));
+
+        }
+
+    }
+    void setupGauntlets() {
+        GauntletSelectLayer::setupGauntlets();
+
+        auto director = CCDirector::sharedDirector();
+        auto winSize = director->getWinSize();
+
+        m_fields->m_dotsMenu = CCMenu::create();
+        m_fields->m_dotsMenu->setLayout(AxisLayout::create());
+        m_fields->m_dotsMenu->setPositionY(director->getScreenBottom() + 15.f);
+        addChild(m_fields->m_dotsMenu);
+
+        for(int i = 0; i < m_scrollLayer->getTotalPages(); i++) {
+            auto spr = CCSprite::createWithSpriteFrameName("gj_navDotBtn_off_001.png");
+            spr->setScale(0.8f);
+
+            CCMenuItemSpriteExtra* btn = CCMenuItemSpriteExtra::create(spr, this, menu_selector(RedesignedGauntletSelectLayer::onDot));
+            m_fields->m_dotsMenu->addChild(btn);
+            m_fields->m_dots.push_back(btn);
+        }
+
+        auto dotsArray = CCArrayExt<CCSprite*>(m_scrollLayer->m_dots);
+
+        for(CCSprite* dot : dotsArray) {
+            dot->setVisible(false);
+        }
+        m_scrollLayer->m_dotsVisible = false;
+
+
+        updateDots();
+
+        m_fields->m_dotsMenu->updateLayout();
+    }
+
+    void onDot(CCObject* sender) {
+        auto btnIdx = std::find(m_fields->m_dots.begin(), m_fields->m_dots.end(), sender) - m_fields->m_dots.begin();
+
+        m_scrollLayer->instantMoveToPage(btnIdx);
+        m_scrollLayer->updatePages();
+        m_scrollLayer->repositionPagesLooped();
+    }
+
+    void scrollLayerScrolledToPage(BoomScrollLayer* p0, int p1) {
+        GauntletSelectLayer::scrollLayerScrolledToPage(p0, p1);
+        updateDots();
+    }
+};
