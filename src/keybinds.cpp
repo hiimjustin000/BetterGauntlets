630da5ad793baaa2b0a14f80db25e43fc4336ae7
Jay <168689600+JayDasshu@users.noreply.github.com>
2024-09-24 19:45:14 -0400

    keybinds...?

--- src/keybinds.cpp
+++ src/keybinds.cpp
@@ -53,7 +53,7 @@ GauntletSelectLayer::onBack(sender);
 void setupGauntlets() {
     GauntletSelectLayer::setupGauntlets();
     if (const auto pageButtons = getChildByIDRecursive("page-buttons")) {
-        MyGauntletSelectLayer::findCurrentGauntletPageUsing(pageButtons);
+        RedesignedGauntletSelectLayer::findCurrentGauntletPageUsing(pageButtons);
     }
 this->defineKeybind("next-gauntlet"_spr, [this]() {
     GauntletSelectLayer::onNext(nullptr); // default: right arrow key
@@ -72,4 +72,44 @@ this->defineKeybind("third-visible-gauntlet"_spr, [this]() {
     });
     }
 };
+#endif
+
+#ifndef GEODE_IS_MACOS
+$execute {
+    BindManager::get()->registerBindable({
+        "previous-gauntlet"_spr,
+        "Previous Gauntlet",
+        "Use this keybind (set to Left by default) to view the previous gauntlet page.",
+        { Keybind::create(KEY_Left) },
+        "Global/Gauntlet Touchup"
+    });
+    BindManager::get()->registerBindable({
+        "next-gauntlet"_spr,
+        "Next Gauntlet",
+        "Use this keybind (set to Right by default) to view the next gauntlet page.",
+        { Keybind::create(KEY_Right) },
+        "Global/Gauntlet Touchup"
+    });
+    BindManager::get()->registerBindable({
+        "first-visible-gauntlet"_spr,
+        "First Gauntlet",
+        "Use this keybind (set to 1 by default) to enter the first visible gauntlet.\n\n<cy>(Disclaimer: Results may not be accurate.)</c>",
+        { Keybind::create(KEY_One) },
+        "Global/Gauntlet Touchup"
+    });
+    BindManager::get()->registerBindable({
+        "second-visible-gauntlet"_spr,
+        "Second Gauntlet",
+        "Use this keybind (set to 2 by default) to enter the second visible gauntlet.\n\n<cy>(Disclaimer: Results may not be accurate.)</c>",
+        { Keybind::create(KEY_Two) },
+        "Global/Gauntlet Touchup"
+    });
+    BindManager::get()->registerBindable({
+        "third-visible-gauntlet"_spr,
+        "Third Gauntlet",
+        "Use this keybind (set to 3 by default) to enter the third visible gauntlet.\n\n<cy>(Disclaimer: Results may not be accurate.)</c>",
+        { Keybind::create(KEY_Three) },
+        "Global/Gauntlet Touchup"
+    });
+}
 #endif
